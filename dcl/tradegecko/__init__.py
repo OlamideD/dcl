747f68dfd9593c625cc996e5af57ecc6c6ced6a9
jvfiel <jvfiel@bai.ph>
2019-05-05 21:45:42 +0800

    PO exchange rate

--- dcl/tradegecko/__init__.py
+++ dcl/tradegecko/__init__.py
@@ -78,8 +78,8 @@ def gecko_po():
     # tg = TradeGeckoRestClient(access_token, refresh_token)
     tg = TradeGeckoRestClient(access_token)
     # print tg.company.all()['companies'][0]
-    # orders = tg.purchase_order.all()['purchase_orders']
-    orders = tg.purchase_order.filter(order_number="PO0443")['purchase_orders']
+    orders = tg.purchase_order.all()['purchase_orders']
+    # orders = tg.purchase_order.filter(order_number="PO0443")['purchase_orders']
 
     # print orders
     income_accounts = "5111 - Cost of Goods Sold - DCL"
